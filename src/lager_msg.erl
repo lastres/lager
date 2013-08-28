0889fb3c6a6f02172f0ed00e3f7449a8b6ca3dd0
Lastres <ramonlastres@gmail.com>
2013-08-28 15:15:01 +0100

    Add a configuration option to not generate timestamps. Timestamp is not added anymore to the message record

--- src/lager_msg.erl
+++ src/lager_msg.erl
@@ -2,7 +2,6 @@
 
 -export([new/4, new/5]).
 -export([message/1]).
--export([timestamp/1]).
 -export([datetime/1]).
 -export([severity/1]).
 -export([severity_as_int/1]).
@@ -14,7 +13,6 @@
         metadata :: [tuple()],
         severity :: lager:log_level(),
         datetime :: {string(), string()},
-        timestamp :: erlang:timestamp(),
         message :: list()
     }).
 
@@ -22,24 +20,29 @@
 -export_type([lager_msg/0]).
 
 %% create with provided timestamp, handy for testing mostly
--spec new(list(), erlang:timestamp(), lager:log_level(), [tuple()], list()) -> lager_msg().
+-spec new(list(), erlang:timestamp() | 'undefined', lager:log_level(), [tuple()], list()) -> lager_msg().
+new(Msg, undefined, Severity, Metadata, Destinations) ->
+    #lager_msg{message=Msg, datetime={"Undefined", "Undefined"}, severity=Severity,
+	       metadata=Metadata, destinations=Destinations};
 new(Msg, Timestamp, Severity, Metadata, Destinations) ->
     {Date, Time} = lager_util:format_time(lager_util:maybe_utc(lager_util:localtime_ms(Timestamp))),
-    #lager_msg{message=Msg, datetime={Date, Time}, timestamp=Timestamp, severity=Severity,
+    #lager_msg{message=Msg, datetime={Date, Time}, severity=Severity,
         metadata=Metadata, destinations=Destinations}.
 
 -spec new(list(), lager:log_level(), [tuple()], list()) -> lager_msg().
 new(Msg, Severity, Metadata, Destinations) ->
-    Now = os:timestamp(),
-    new(Msg, Now, Severity, Metadata, Destinations).
+    Timestamp =
+	case application:get_env(lager, use_timestamp) of
+	    {ok, false} ->
+		undefined;
+	    _ ->
+		os:timestamp()
+	end,
+    new(Msg, Timestamp, Severity, Metadata, Destinations).
 
 -spec message(lager_msg()) -> list().
 message(Msg) ->
     Msg#lager_msg.message.
-
--spec timestamp(lager_msg()) -> erlang:timestamp().
-timestamp(Msg) ->
-    Msg#lager_msg.timestamp.
 
 -spec datetime(lager_msg()) -> {string(), string()}.
 datetime(Msg) ->
